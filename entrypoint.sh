fade3c1e1982c19f69ae1e3cc84722fafe4b22a6
Lyndon Maydwell <lyndon@sordina.net>
2023-09-13 13:29:51 +1000

    cleaning up infer and putting some functions into procedures instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ typescript_source=$(jq -r .typescript_source </config.json)
 typescript_directory=$(dirname "$typescript_source")
 
 cp src/server.ts "$typescript_directory"
-cp infer.ts "$typescript_directory"
+cp src/infer.ts "$typescript_directory"
 cp "$typescript_source" "$typescript_directory"/funcs.ts
 
 (
@@ -16,18 +16,4 @@ cp "$typescript_source" "$typescript_directory"/funcs.ts
   $HOME/.deno/bin/deno run --allow-env --allow-net server.ts & # Server
 )
 
-# Is the config available to the entrypoint?
-# Is that passed in with --ENV
-
 "$@"
-
-# 3. Fix entrypoint
-#   3.a. Run inference on actual typescript sources
-#     - Env var?
-#     - Vendor deps
-#   3.b. Run the deno server serving the functions in background
-#     - Package functions with server shim
-#   3.c. Run typescript connector binary in foreground
-# 4. Remove sending things to deno
-# 5. Test with localhost as deno host
-# 6. Test with connector create
