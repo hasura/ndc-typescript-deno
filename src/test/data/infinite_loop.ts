f1f05c76ec879d70a1f10bf1bad8488e8af09557
Lyndon Maydwell <lyndon@sordina.net>
2023-10-12 09:45:58 +1000

    path dep

--- src/test/data/infinite_loop.ts
+++ src/test/data/infinite_loop.ts
@@ -1,5 +1,5 @@
 
-// Infinite loop bug: https://github.com/hasura/ndc-typescript-deno/issues/45
+// Infinite loop void bug: https://github.com/hasura/ndc-typescript-deno/issues/45
 
 export async function infinite_loop() {
   const contents = await fetch('https://www.google.com')
