ed7a194d0116f73cdca64b4a8d0f5f8efbb522a9
Lyndon Maydwell <lyndon@sordina.net>
2023-11-23 09:12:18 +1000

    changing TS dependencies to fix bug reported by sooraj

--- src/infer.ts
+++ src/infer.ts
@@ -371,11 +371,11 @@ export function programInfoException(filename_arg?: string, vendor_arg?: string,
   `);
 
   const program = ts.createProgram([filename], {
-    target: ts.ScriptTarget.ES5,
+    target: ts.ScriptTarget.ES2022,
     module: ts.ModuleKind.CommonJS,
     noImplicitAny: true,
     // NOTE: We just declare Deno globally as any in order to allow users to omit it's declaration in their function files
-    lib: ['lib.d.ts', 'lib.es2017.d.ts', resolve(deno_d_ts)],
+    lib: ['lib.d.ts', 'lib.es2022.d.ts',  resolve(deno_d_ts)],
     allowJs: true,
     allowImportingTsExtensions: true,
     noEmit: true,
@@ -401,7 +401,7 @@ export function programInfoException(filename_arg?: string, vendor_arg?: string,
         const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
         console.error(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
       } else {
-        console.error(ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n"));
+        console.error(`FATAL: ${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`);
         fatal++;
       }
     });
